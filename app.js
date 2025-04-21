d6523f0d77dedfaf1e0efac47f89599e966cb89d
César Guillermo Nevárez <memonevarez@hotmail.com>
2025-04-21 23:36:03 +0100

    database folder structure

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
-// const cors = require("cors");
+const cors = require("cors");
 // const {
 //   psqlErrorHandler,
 //   customErrorHandler,
 //   serverErrorHandler,
 // } = require("./error-handlers/error-handlers");
 
-// app.use(cors());
 //Middleware
 app.use(express.json()); // Only when we post
+app.use(cors());
 
 //Routes
 app.get("/", (req, res) => res.send("please use /api"));
---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS nc_launchpad_test;
+DROP DATABASE IF EXISTS nc_launchpad;
+
+CREATE DATABASE nc_launchpad_test;
+CREATE DATABASE nc_launchpad;
